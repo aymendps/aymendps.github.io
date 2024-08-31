44e7e98f62fa87184c54dbc1b1bd6b0a98e84b16
Aymen Hammami <hammami.aym@outlook.com>
2024-08-31 13:58:20 +0100

    removed warning

--- src/components/Projects/ProjectSlider.js
+++ src/components/Projects/ProjectSlider.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Tabs, Tab, Box, Typography } from "@mui/material";
 import { styled } from "@mui/system";
 import { getProjectsNumberByIndustry } from "../ProjectList";
