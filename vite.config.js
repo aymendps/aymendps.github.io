d6b8b5f16a71b73d4ab92ec400cabb3579d06858
Aymen Hammami <hammami.aym@outlook.com>
2026-05-10 16:01:03 +0100

    attempt for browser router 2

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
   base: "/",
-  plugins: [react(), tailwindcss(), ghPages()],
-  build: {
-    outDir: "build", // CRA's default build output
-  },
+  plugins: [react(), tailwindcss()],
 });
