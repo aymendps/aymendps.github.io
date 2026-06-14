9efe6235c3acc25ec71959370c86127d88e8f4a6
Aymen Hammami <hammami.aym@outlook.com>
2026-06-14 17:36:56 +0100

    improve ui of pictures viewer

--- src/components/Projects/Shared/PicturesViewer.jsx
+++ src/components/Projects/Shared/PicturesViewer.jsx
@@ -1,5 +1,5 @@
 import { ArrowLeft, ArrowRight, OpenInNew } from "@mui/icons-material";
-import { IconButton, Typography } from "@mui/material";
+import { CircularProgress, IconButton, Typography } from "@mui/material";
 import { useState, useRef } from "react";
 import { preload } from "react-dom";
 
@@ -36,21 +36,28 @@ function PicturesViewer({ picturesURL = [] }) {
 
   return (
     <div
-      className="w-full aspect-video bg-darkblue border-2 border-darkblue relative flex justify-center items-center select-none"
+      className="w-full aspect-video bg-darkblue overflow-hidden border-2 rounded-xl border-darkblue relative flex justify-center items-center select-none"
       onMouseDown={handleTouchStart}
       onMouseUp={handleTouchEnd}
       onTouchStart={handleTouchStart}
       onTouchEnd={handleTouchEnd}
     >
+      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center z-1">
+        <CircularProgress
+          className="text-cyan-600 z-1"
+          size={90}
+          thickness={1}
+        />
+      </div>
       <img
         key={"image-" + picturesURL[currentPicture]}
         src={picturesURL[currentPicture]}
         alt=""
-        className="w-full h-full object-contain select-none pointer-events-none"
+        className="z-5 w-full h-full object-contain select-none pointer-events-none"
       />
       {currentPicture < picturesURL.length - 1 && (
         <IconButton
-          className="absolute right-4 top-[50%] translate-y-[-50%] text-white bg-black bg-opacity-50 hover:bg-opacity-100 p-0 focus:bg-opacity-50"
+          className="z-5 absolute right-4 top-[50%] translate-y-[-50%] text-white bg-black bg-opacity-50 hover:bg-opacity-100 p-0 focus:bg-opacity-50"
           onClick={() => {
             setCurrentPicture((prev) => prev + 1);
           }}
@@ -60,7 +67,7 @@ function PicturesViewer({ picturesURL = [] }) {
       )}
       {currentPicture > 0 && (
         <IconButton
-          className="absolute left-4 top-[50%] translate-y-[-50%] text-white bg-black bg-opacity-50 hover:bg-opacity-100 p-0 focus:bg-opacity-50"
+          className="z-5 absolute left-4 top-[50%] translate-y-[-50%] text-white bg-black bg-opacity-50 hover:bg-opacity-100 p-0 focus:bg-opacity-50"
           onClick={() => {
             setCurrentPicture((prev) => prev - 1);
           }}
@@ -68,14 +75,14 @@ function PicturesViewer({ picturesURL = [] }) {
           <ArrowLeft fontSize="large" />
         </IconButton>
       )}
-      <div className="absolute bottom-4 left-[50%] translate-x-[-50%] flex flex-row items-center justify-center gap-2">
-        <div className="bg-black bg-opacity-50 text-white px-2 py-1 rounded-xl">
+      <div className="z-5 absolute bottom-4 left-[50%] translate-x-[-50%] flex flex-row items-center justify-center gap-2">
+        <div className="z-5 bg-black bg-opacity-50 text-white px-2 py-1 rounded-xl">
           <Typography>
             {currentPicture + 1} / {picturesURL.length}
           </Typography>
         </div>
         <IconButton
-          className="text-white bg-black bg-opacity-50 hover:bg-opacity-100 focus:bg-opacity-50"
+          className="z-5 text-white bg-black bg-opacity-50 hover:bg-opacity-100 focus:bg-opacity-50"
           onClick={() => {
             window.open(picturesURL[currentPicture], "_blank");
           }}
